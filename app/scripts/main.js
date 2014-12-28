f5baf155a663aadddc8ae24e85bc9c113c8b3982
jennaleeb <jennaleeblumenthal@gmail.com>
2014-12-28 11:35:30 -0500

    Started prototyping dropdown list for specific food types

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,16 @@
-'use strict';
+// 'use strict';
 
-(function($) {
-  $(document).ready(function() {
-    // Add your jQuery code here
-  });
-})(jQuery);
+// (function($) {
+//   $(document).ready(function() {
+//     // Add your jQuery code here
+//   });
+// })(jQuery);
+
+$(document).ready(function() {
+	$(".food-specific").hide();
+	$(".food-family").click(function() {
+		$(".food-specific").toggle();
+	});
+
+	$("input").prop("checked")
+});
