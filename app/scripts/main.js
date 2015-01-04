789d1683b0deaf995f98cef52f6e2f39e84c1f04
jennaleeb <jennaleeblumenthal@gmail.com>
2015-01-04 15:15:43 -0500

    Section to type food preferences and print on screen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,34 @@
-// 'use strict';
+'use strict';
 
-// (function($) {
-//   $(document).ready(function() {
-//     // Add your jQuery code here
-//   });
-// })(jQuery);
-
-$(document).ready(function() {
-	$(".food-specific").hide();
-	$(".food-family").click(function() {
-		$(".food-specific").toggle();
+(function($) {
+  
+// Add text in textbox as list of food item
+	$('input[type=button]').on('click', function() {
+		var item = $('input[type=text]').val();
+		if (item.length !== 0) {
+			$( ".cant-eat-container" ).append( '<h3 class="list-of-items">'+item+'</h3>' );
+			$('input[type=text]').val('');
+		}
+    	
 	});
 
-	$("input").prop("checked")
-});
+// Use enter key as button click
+	$('#food_adder').keypress(function (e) {
+ 		var key = e.which;
+ 		if(key == 13)  // the enter key code
+  			{
+			    $('input[type=button]').click();
+			    return false;  
+  			}
+	});  
+
+	
+
+ //  	$(".food-specific").hide();
+	// $(".food-family").click(function() {
+	// 	$(".food-specific").toggle();
+	// });
+
+	// $("input").prop("checked")
+
+})(jQuery);
