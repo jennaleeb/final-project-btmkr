d3371775d3742cb83dd6a5006f09974df66201ad
jennaleeb <jennaleeblumenthal@gmail.com>
2015-01-13 18:31:06 -0500

    Working on changing colour based on "don't like/can't eat"

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,27 @@
 'use strict';
 
+
 (function($) {
   
 // Add text in textbox as list of food item
+	$('.food_adder').fadeIn(1000);
 	$('input[type=button]').on('click', function() {
-		var item = $('input[type=text]').val();
+		var item = $('.food_adder').val();
+		var xMark = '<div class="delete dont-like">~</div>';
+		var exclaimMark = '<div class="delete cant-eat">!</div>';
+		var heightAdd = 250;
 		if (item.length !== 0) {
-			$( ".cant-eat-container" ).append( '<h3 class="list-of-items">'+item+'</h3>' );
-			$('input[type=text]').val('');
+			$( ".cant-eat-container" ).append( '<li class="list-of-items">'+item+xMark+exclaimMark+'</li>');
+			$('.food_adder').val('');
+			$('.cant-eat-section').css('height',heightAdd);
+			
 		}
+
     	
 	});
 
 // Use enter key as button click
-	$('#food_adder').keypress(function (e) {
+	$('.food_adder').keypress(function (e) {
  		var key = e.which;
  		if(key == 13)  // the enter key code
   			{
@@ -22,6 +30,17 @@
   			}
 	});  
 
+// Change colour of food item when click ~ or !
+	$('.dont-like').on('click', function() {
+		$(this).closest('.list-of-items').toggleClass('dont-like-background');
+	});
+
+	$('.cant-eat').on('click', function() {
+		$(this).closest('.list-of-items').toggleClass('cant-eat-background');
+	
+
+
+	});
 	
 
  //  	$(".food-specific").hide();
